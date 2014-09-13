50ddcf31016e34c4977dcf45f1ae7942c800bb36
Andrew Rymar <anr@qapint.com>
2014-09-13 11:50:05 +0300

    Change views to be more usable

--- helpers/settings.rb
+++ helpers/settings.rb
@@ -1,13 +1,11 @@
 class Settings
-  attr_reader :vk_login, :vk_password
+  @@settings = YAML::load_file(File.expand_path('../../config/settings.yml', __FILE__))
 
-  @@settings = YAML::load_file('../config/config.yml')
-
-  def vk_login
+  def self.vk_login
     @@settings['vk_login']
   end
 
-  def vk_password
+  def self.vk_password
     @@settings['vk_password']
   end
 end
